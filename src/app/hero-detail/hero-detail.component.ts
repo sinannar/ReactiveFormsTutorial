4bf647ceaa1d162260e9429fb5ead6b187141173
sinan nar <sinan.nar@gmail.com>
2018-04-04 16:51:35 +1200

    implemented until data mode land form model

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { states } from '../data-model';
+
+// https://angular.io/guide/reactive-forms#the-data-model-and-the-form-model
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  heroForm: FormGroup;
+  states = states;
+  constructor(private _builder: FormBuilder) {
+    this.createForm();
+  }
+
+  ngOnInit() {
+  }
+
+  createForm() {
+    this.heroForm = this._builder.group({ // <-- the parent FormGroup
+      name: ['', Validators.required ],
+      address: this._builder.group({ // <-- the child FormGroup
+        street: '',
+        city: '',
+        state: '',
+        zip: ''
+      }),
+      power: '',
+      sidekick: ''
+    });
+  }
+}
